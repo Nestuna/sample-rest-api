70d9955f46fd5015e9d435940bccf915273aec0a
DE SOUSA Dylan <dds1991@hotmail.fr>
2021-09-29 09:27:59 +0200

    Adding Prisma models + migrations

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const api = Router()
 
 /** GET /api/users > Retrieve all users */
 api.get('/', (_, response) => {
-  response.json({ data: { users: [] } })
+  const prisma = new PrismaClient()
+
+  response.json({ data: { users: prisma.user.findMany() } })
 })
 
 export default api
