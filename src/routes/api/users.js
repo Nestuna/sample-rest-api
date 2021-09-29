613edbdde2f3ba4c68bbd6d65e50e8b037898556
nantunes <nicolas.antunes@ubicast.eu>
2021-09-29 14:03:41 +0200

    init repo and add CRUD requests with prisma

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,12 +2,61 @@ import { Router } from 'express'
 import { PrismaClient } from '@prisma/client'
 
 const api = Router()
+const prisma = new PrismaClient()
 
 /** GET /api/users > Retrieve all users */
-api.get('/', (_, response) => {
-  const prisma = new PrismaClient()
+api.get('/', async (_, response) => {
+  response.json({ data: { users: await prisma.user.findMany() } })
+})
 
-  response.json({ data: { users: prisma.user.findMany() } })
+/** POST /api/users > Create an user */
+api.post('/', async (request, response) => {
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        email: request.body.email,
+        username: request.body.username
+      }
+    })
+    return response.json({data: {message: `User ${newUser.email} created`}})
+  } catch(e) {
+    console.error(`Failed to create user : ${e}`)
+    return response.json({data: {message: 'Failed to create user'}})
+  }
+})
+
+/** PUT /api/users/ > modify user */
+api.put('/', async (request, response) => {
+  try {
+    const { username, email  } = request.body
+    await prisma.user.update({
+      where: {
+        email: email
+      },
+      data: {
+        username: username
+      }
+    })
+    return response.json({data : {message: `Username of ${email} modified`}})
+  } catch(e) {
+    console.error(`Failed modify use ${email}`)
+    return response.json({data: {message: `Failed to modify username for ${email}`}})
+  }
+})
+
+api.delete('/', async (request, response) => {
+  try {
+    const { email  } = request.body
+    await prisma.user.delete({
+      where: {
+        email: email
+      }
+    })
+    return response.json({data : {message: `${email} deleted`}})
+  } catch(e) {
+    console.error(`Failed modify use ${email}`)
+    return response.json({data: {message: `${email} deleted`}})
+  }
 })
 
 export default api
--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ import routes from './routes'
 export async function launch(port = 4242) {
   const application = express()
 
+  application.use(express.json())
   /** Routes */
   application.use('/', routes)
-    
+
   application.listen(port, () => {
     console.log(`API started at: http://localhost:${port}/api`)
   })
